f20657864e3c58a1b1f577ac658e5b1f6a89c50d
Halima Lena Camara <halimalenac@gmail.com>
2026-02-18 22:51:58 +0000

    AsyncPype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/private/header/header.component';
 import { Observable, Subscriber, Subscription } from 'rxjs';
 import { subscribe } from 'diagnostics_channel';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnDestroy,OnInit {
   title = 'mon-projet';
-  letter: string = '';
-  letter1: string = '';
-  letter2: string = '';
-
-  susbcription1?: Subscription; 
-  susbcription2?: Subscription; 
-  susbcription3?: Subscription; 
 
   helloObservables?:Observable<string>
 
@@ -46,42 +40,10 @@ export class AppComponent implements OnDestroy,OnInit {
 
   ngOnInit(): void {
 
-    const helloObserver1 = {
-      next: (letter: string) => {
-        this.letter = letter;
-      },
-      complete: () => {
-        console.log("Observer 1 a reçu toutes les données");
-      }
-    };
-  
-    const helloObserver2 = {
-      next: (letter: string) => {
-        this.letter1 = letter;
-      },
-      complete: () => {
-        console.log("Observer 2 a reçu toutes les données");
-      }
-    };
-  
-    this.susbcription1 = this.helloObservables!.subscribe(helloObserver1);
-    this.susbcription2 = this.helloObservables!.subscribe(helloObserver2);
-    this.susbcription3 = this.helloObservables!.subscribe({
-      next: (letter: string) => {
-        this.letter2 = letter;
-      },
-      complete: () => {
-        console.log("Observer 3 a reçu toutes les données");
-      }
-    });
   }
 
   ngOnDestroy(): void {
-    this.susbcription1?.unsubscribe();
-    this.susbcription2?.unsubscribe();
-    this.susbcription3?.unsubscribe();
-    console.log("Tous les abonnements ont été désabonnés");
-   
+    
   }
   
 }
